9a337b51910eecc7f98ce33e14ca5fc542af16fc
Derek Hu <derek.dchu@gmail.com>
2014-12-12 03:24:42 -0500

    Setup allauth and Email server in dev settings.

--- djangular_2048/settings/dev.py
+++ djangular_2048/settings/dev.py
@@ -6,6 +6,52 @@ DEBUG = True
 
 INSTALLED_APPS = DEFAULT_APPS + (
     'game',
+
+    # The Django sites framework is required by allauth
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.twitter',
 )
 
 MIDDLEWARE_CLASSES = DEFAULT_MIDDLEWARE_CLASSES + ()
+
+TEMPLATE_CONTEXT_PROCESSORS = TEMPLATE_CONTEXT_PROCESSORS + (
+
+    # allauth specific context processors
+    'allauth.account.context_processors.account',
+    'allauth.socialaccount.context_processors.socialaccount',
+)
+
+AUTHENTICATOIN_BACKENDS = (
+    'django.contrib.auth.backend.ModelBackend'
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 2
+
+# Allauth settings
+# auth and allauth settings
+LOGIN_REDIRECT_URL = '/'
+SOCIALACCOUNT_QUERY_EMAIL = True
+SOCIALACCOUNT_PROVIDERS = {
+    'facebook': {
+        'SCOPE': ['email', 'publish_stream'],
+        'METHOD': 'oauth2'  # instead of 'js_sdk'
+    }
+}
+
+# Email server
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'investnotice@gmail.com'
+EMAIL_HOST_PASSWORD = 'investnotice123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'admin@djangular-2048.com'
