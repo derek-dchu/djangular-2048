eb499d5c17e8e5c4fb174e612d18aeaa51489241
Derek Hu <derek.dchu@gmail.com>
2014-12-14 16:53:01 -0500

    Add urls for auth and python-social-auth apps.

--- djangular_2048/urls.py
+++ djangular_2048/urls.py
@@ -4,4 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', include('game.urls', namespace='game-app', app_name="game")),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^accounts/', include('django.contrib.auth.urls', namespace='auth')),
+    url(r'^social/', include('social.apps.django_app.urls', namespace='social', app_name='python-social-auth')),
 )
