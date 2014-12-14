efe9807fccfb776add56e2f945c5ff64d351c9d6
Derek Hu <derek.dchu@gmail.com>
2014-12-14 04:13:35 -0500

    Create settings for python-social-auth.

--- djangular_2048/settings/default.py
+++ djangular_2048/settings/default.py
@@ -153,11 +153,3 @@ TEMPLATE_DIRS = (
 #### END TEMPLATE CONFIGURATION
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-
-try:
-    from .local_settings import *
-except ImportError:
-    import sys, traceback
-    sys.stderr.write("Warning: Can't find the file 'local_settings.py' in the directory containing {}. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n".format(__file__))
-    sys.stderr.write("\nFor debugging purposes, the exception was:\n\n")
-    traceback.print_exc()
--- djangular_2048/settings/dev.py
+++ djangular_2048/settings/dev.py
@@ -5,7 +5,30 @@ from .default import *
 DEBUG = True
 
 INSTALLED_APPS = DEFAULT_APPS + (
+    'social.apps.django_app.default',
+
     'game',
 )
 
 MIDDLEWARE_CLASSES = DEFAULT_MIDDLEWARE_CLASSES + ()
+
+TEMPLATE_CONTEXT_PROCESSORS = TEMPLATE_CONTEXT_PROCESSORS + (
+    'social.apps.django_app.context_processors.backends',
+   'social.apps.django_app.context_processors.login_redirect',
+)
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'social.backends.facebook.FacebookOAuth2',
+    'social.backends.google.GoogleOAuth2',
+    'social.backends.twitter.TwitterOAuth',
+)
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    import sys, traceback
+    sys.stderr.write("Warning: Can't find the file 'local_settings.py' in the directory containing {}. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n".format(__file__))
+    sys.stderr.write("\nFor debugging purposes, the exception was:\n\n")
+    traceback.print_exc()
