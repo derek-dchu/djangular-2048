ca2d5ba60b405bd49c8c71af96ca1b7070f3c916
Derek Hu <derek.dchu@gmail.com>
2014-12-14 04:12:55 -0500

    Update urls setting.

--- djangular_2048/urls.py
+++ djangular_2048/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', include('game.urls')),
+    url(r'^$', include('game.urls', namespace='game-app', app_name="game")),
     url(r'^admin/', include(admin.site.urls)),
 )
