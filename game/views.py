e152fac8ef137234f097f52fc20dbe3fb0dfd3b5
Derek Hu <derek.dchu@gmail.com>
2014-12-14 21:50:54 -0500

    Move login require login into view by using LoginRequiredMixin provided by django-braces.

--- game/views.py
+++ game/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
+from django.core.urlresolvers import reverse_lazy
 
+from braces.views import LoginRequiredMixin
 
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'game/index.html'
+
+    login_url = reverse_lazy('account:account_login')
