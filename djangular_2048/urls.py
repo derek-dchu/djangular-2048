2fdc2042ff142f659cd9974ba145cc06f9b299c2
Derek Hu <derek.dchu@gmail.com>
2014-12-14 19:37:41 -0500

    Add default auth urls, because python-user-accounts needs it to authenticate login.

--- djangular_2048/urls.py
+++ djangular_2048/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', include('game.urls', namespace='game-app', app_name="game")),
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^accounts/', include('account.urls', namespace='accounts', app_name='python-user-accounts')),
+    url(r'^auth/', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^social/', include('social.apps.django_app.urls', namespace='social', app_name='python-social-auth')),
+    url(r'^', include('account.urls', namespace='accounts', app_name='python-user-accounts')),
 )
