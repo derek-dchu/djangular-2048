4545b417839cb2010b2812b0f82aa549474a31f4
Derek Hu <derek.dchu@gmail.com>
2014-12-14 20:52:50 -0500

    Install django sites app for manging site information. Setup email server.

--- djangular_2048/settings/dev.py
+++ djangular_2048/settings/dev.py
@@ -5,6 +5,7 @@ from .default import *
 DEBUG = True
 
 INSTALLED_APPS = DEFAULT_APPS + (
+    'django.contrib.sites',
 
     'account',
     'social.apps.django_app.default',
@@ -45,6 +46,17 @@ LOGIN_REDIRECT_URL = '/'
 #### Python-User-Accounts settings
 ACCOUNT_EMAIL_UNIQUE = True
 ACCOUNT_EMAIL_CONFIRMATION_REQUIRED = True
+
+
+SITE_ID = 2
+#### Setup Email server
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'investnotice@gmail.com'
+EMAIL_HOST_PASSWORD = 'investnotice123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'admin@djangular-2048.com'
 
 
 try:
