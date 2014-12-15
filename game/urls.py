12dee49015c1637328883b06a0763cf27557260d
Derek Hu <derek.dchu@gmail.com>
2014-12-14 20:03:27 -0500

    Add name for game IndexView.

--- game/urls.py
+++ game/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from .views import IndexView
 
 urlpatterns = patterns('',
-    url(r'^$', IndexView.as_view())
+    url(r'^$', IndexView.as_view(), name='index')
 )
