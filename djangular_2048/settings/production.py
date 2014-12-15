0c16e890f02c588cca628668146673a6054b4d14
Derek Hu <derek.dchu@gmail.com>
2014-12-15 00:36:27 -0500

    Update production settings for deployment.

--- djangular_2048/settings/production.py
+++ djangular_2048/settings/production.py
@@ -7,20 +7,32 @@ from .default import *
 DEBUG = False
 
 INSTALLED_APPS = DEFAULT_APPS + (
+    'django.contrib.sites',
+
+    'account',
     'social.apps.django_app.default',
+    'bootstrap3',
 
     'game',
 )
 
-MIDDLEWARE_CLASSES = DEFAULT_MIDDLEWARE_CLASSES + ()
+MIDDLEWARE_CLASSES = DEFAULT_MIDDLEWARE_CLASSES + (
+    'account.middleware.LocaleMiddleware',
+    'account.middleware.TimezoneMiddleware',
+)
 
 TEMPLATE_CONTEXT_PROCESSORS = TEMPLATE_CONTEXT_PROCESSORS + (
+    'account.context_processors.account',
+
     'social.apps.django_app.context_processors.backends',
    'social.apps.django_app.context_processors.login_redirect',
 )
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
+
+    'account.auth_backends.EmailAuthenticationBackend',
+
     'social.backends.facebook.FacebookOAuth2',
     'social.backends.google.GoogleOAuth2',
     'social.backends.twitter.TwitterOAuth',
@@ -59,3 +71,19 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET
 # Twitter
 SOCIAL_AUTH_TWITTER_KEY = environ.get('SOCIAL_AUTH_TWITTER_KEY')
 SOCIAL_AUTH_TWITTER_SECRET = environ.get('SOCIAL_AUTH_TWITTER_SECRET')
+
+
+#### Python-User-Accounts settings
+ACCOUNT_EMAIL_UNIQUE = True
+ACCOUNT_EMAIL_CONFIRMATION_REQUIRED = True
+
+
+SITE_ID = 1
+#### Setup Email server
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'investnotice@gmail.com'
+EMAIL_HOST_PASSWORD = 'investnotice123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'admin@djangular-2048.com'
